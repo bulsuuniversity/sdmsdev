48a93fd519a4813d673d90ce3dbce23d6bd12378
AaronAnablon <anablonaaronp@gmail.com>
2023-09-17 19:27:03 +0800

    ss

--- src/app/libs/prismadb.js
+++ src/app/libs/prismadb.js
@@ -1,6 +1,15 @@
+// import {PrismaClient} from '@prisma/client';
+
+// const client = globalThis.prisma || new PrismaClient();
+// if(process.env.NODE_ENV !== 'production') globalThis.prisma = client;
+
+// export default client;
+
+
 import {PrismaClient} from '@prisma/client';
 
-const client = globalThis.prisma || new PrismaClient();
-if(process.env.NODE_ENV !== 'production') globalThis.prisma = client;
+global.prisma = global.prisma || new PrismaClient({ log: ["info"] });
 
-export default client;
+if (process.env.NODE_ENV !== "production") global.prisma = global.prisma;
+
+module.exports = global.prisma;
